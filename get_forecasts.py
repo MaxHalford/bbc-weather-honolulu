dcdfb94dc6e6f75ed87a8e542b16ca8059f53062
Max Halford <maxhalford25@gmail.com>
2021-03-08 10:09:31 +0100

    reset

---
+++ get_forecasts.py
@@ -0,0 +1,16 @@
+import json
+
+with open('forecast.json') as f:
+    forecast = json.load(f)
+
+for f in forecast['forecasts']:
+    del f['summary']
+    for r in f['detailed']['reports']:
+        del r['enhancedWeatherDescription']
+        del r['precipitationProbabilityText']
+        del r['weatherTypeText']
+        del r['windDescription']
+        del r['windDirectionFull']
+
+with open('forecasts.json', 'w') as f:
+    json.dump(obs, f, indent=4, sort_keys=True)
